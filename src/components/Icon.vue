6cd779a239e70344e475c72209a537c0a9386bef
LiendoJulian <jnliendo1999@gmail.com>
2023-11-18 20:23:37 -0300

    feat: adding Components structure

---
+++ src/components/Icon.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div class="icon" v-html="this.getSVG" />
+  </div>
+</template>
+
+<script setup>
+import world from "../assets/world.svg";
+import vue from "../assets/vue.svg";
+
+let icons = {
+  world,
+  vue
+}
+export default {
+  name: "Icon",
+  props: {
+    iconName: {
+      type: String,
+      required: true
+    }
+  },
+
+  get getSVG() {
+    return icons[this.iconName];
+  }
+}
+
+
+
+</script>
+
+<style scoped>
+.icon {
+  width: 35px;
+  height: 35px;
+}
+</style>
